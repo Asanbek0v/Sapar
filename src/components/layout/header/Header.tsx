040fffcfb789106273de71e72b0eb675d6fbc8c7
Дастан <dastan@example.com>
2025-12-14 18:51:27 +0600

    ubdate header tourDetail footer

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -5,15 +5,30 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
+import { Moon, Sun } from "lucide-react";
 
 const Header: FC = () => {
   const router = useRouter();
   const [fav, setFav] = useState(false);
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+    if (isDark) {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+  };
 
   return (
-    <header className="absolute top-8 left-0 w-full z-20">
+    <header className="fixed top-8 left-0 w-full z-99">
       <div className="container">
-        <div className="mx-auto flex items-center justify-between py-2 bg-white rounded-[35px] shadow-lg">
+        <div
+          className="mx-auto flex items-center justify-between py-2 
+                bg-white/100 backdrop-blur-sm 
+                rounded-[35px] shadow-lg"
+        >
           <Image
             src={Headerlogo}
             alt="sapar.kg"
@@ -46,7 +61,6 @@ const Header: FC = () => {
           </nav>
 
           <div className="flex items-center gap-6 text-white mr-6">
-            {/* SEARCH */}
             <div className="relative">
               <input
                 type="text"
@@ -64,19 +78,18 @@ const Header: FC = () => {
                 <circle cx="11" cy="11" r="8" />
               </svg>
             </div>
-
-            {/* LANGUAGE */}
             <select className="focus:outline-none bg-white text-black rounded-lg px-2 py-1 text-[13px]">
               <option value="ru">RU</option>
               <option value="en">EN</option>
             </select>
-
-            <button className="px-5 py-1.5 bg-orange-500 text-white rounded-lg text-[15px] hover:bg-orange-600 transition">
+            <button className="px-5 py-1.5 bg-orange-500/100 text-white rounded-lg text-[15px] hover:bg-orange-600 transition">
               Войти
             </button>
-
             <motion.svg
-              onClick={() => setFav(!fav)}
+              onClick={() => {
+                router.push("/favorite");
+                setFav(!fav);
+              }}
               initial={false}
               animate={{
                 scale: fav ? [1, 1.5, 1.2, 1] : 1,
@@ -93,11 +106,27 @@ const Header: FC = () => {
             >
               <path
                 d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 
-                2 5.42 4.42 3 7.5 3c1.74 0 3.41 1.01 4.5 2.09 
-                C13.09 4.01 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5 
-                c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                 2 5.42 4.42 3 7.5 3c1.74 0 3.41 1.01 4.5 2.09 
+                 C13.09 4.01 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5 
+                 c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
               />
             </motion.svg>
+
+            <div
+              className="flex items-center cursor-pointer text-[#ffffff]"
+              onClick={toggleTheme}
+            >
+              <Sun
+                className={`w-7 h-7 transition-all duration-300 ${
+                  isDark ? "hidden" : "block"
+                }`}
+              />
+              <Moon
+                className={`w-6 h-6 transition-all duration-300 ${
+                  isDark ? "block" : "hidden"
+                }`}
+              />
+            </div>
           </div>
         </div>
       </div>
